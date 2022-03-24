f285d94c03540384f506229087fe5e87bde64629
Jin <kairos0@cu.ac.kr>
2022-03-24 21:36:41 +0900

    Modify database backup script

--- scripts/getDB.sh
+++ scripts/getDB.sh
@@ -1,5 +1,5 @@
 
 
 # sudo permission required
-sudo -kS docker exec oj-postgres sh -c "pg_dump -U onlinejudge onlinejudge > backup.sql; exit"
-sudo -kS docker cp oj-postgres:backup.sql ~/Database_backup/$(date +%y)_$(date +%m)_$(date +%d).sql
+docker exec oj-postgres sh -c "pg_dump -U onlinejudge onlinejudge > backup.sql; exit"
+docker cp oj-postgres:backup.sql ~/Database_backup/$(date +%y)_$(date +%m)_$(date +%d).sql
